12e7f83eedf4dacff460321799a54dba9edd2dc7
Yoshikazu Noda <noda.yoshikazu@gmail.com>
2025-03-10 23:30:40 +0900

    #1

--- src/fibo.py
+++ src/fibo.py
@@ -4,17 +4,18 @@
 #
 #   written by chatgpt
 #
+class Fibb:
+    def fibonacci(self, n):
+        if n <= 0:
+            return "Invalid input"
+        elif n == 1:
+            return 0
+        elif n == 2:
+            return 1
+        else:
+            return fibonacci(n - 1) + fibonacci(n - 2)
 
-def fibonacci(n):
-    if n <= 0:
-        return "Invalid input"
-    elif n == 1:
-        return 0
-    elif n == 2:
-        return 1
-    else:
-        return fibonacci(n - 1) + fibonacci(n - 2)
-
-# Print first 10 Fibonacci numbers
-for i in range(1, 11):
-    print(fibonacci(i), end=" ")
+    def test(self):
+        # Print first 10 Fibonacci numbers
+        for i in range(1, 11):
+            print(fibonacci(i), end=" ")
